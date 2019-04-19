008fdc12962e2d6962b675dc500be2b38d7b1bd5
bible36 <46374100+bible36@users.noreply.github.com>
2019-04-18 18:04:02 -0700

    Add files via upload

---
+++ love death demo/saves/love death demo.bat
@@ -0,0 +1,235 @@
+@echo off
+color 4
+echo    `..`...`  `               `..::///:-..       .......`          ..`.... `...``....`....``...`   
+echo  `.-oyyyhyy/```  `         `-+oyhmmmmmddhyo/``  `yyyyyyy:-  `    :/yyyyyso`.yyyyyyyyyyyyyyyyhys:   
+echo   -+dMMMMMNo      ``     `:odNNNNMMMMMNNMMNmy:`  mNMNNMMso  ``   syMMMMNyo -NMMMNNNMMMMNMMMMMMN+   
+echo   ./dMMMMNN+  ```        .dmMMMNNMMNNNMMNNMMNhs. syMMNMMdh       dmMMMMM/. -MMMMNMMMMMMMMMMMMMN+   
+echo   .+dMNNMMNo`    `` ``  /oMMMMMMNo/`..oyNMMMMMN- -+MMMMNMN`    ``NNNMMNN- `:NNNMMMMys++++++++//.   
+echo   .+dMMMMMN+            syMNmNMmd     `.NMNNNMM:.`-NNMMNMN-.   -:NMNMMdy.  -MMMMMMN:-              
+echo   .+dMMMMMN+            yhMMMMMdh  `  ``NMMMMMM/- .hmMMMMNo/   +sMMMMMy/` `-NNNMMMN-.```           
+echo  -+dMNMMMN+  `    `    yhMMMMMdh     ``NNMMMMN/- `+yMMMMMds   smMMMMNo.  `:NMMMMMN/:-----..       
+echo   .+dMMMMMN+            yhMMNMMdh     ``NMMMMMM/- `.oMMMMMNy ``hNMMMNd/    -NNMMNMNdddhhddys       
+echo  .+dMMMMMN+            yhMMMMMdh     ``NMMMMMM/-   /mNMMMMh`.-dMNNMdo-    -NMMMMNNMMMMMMMNd  `    
+echo   .+dMMMMMN+            yhMMNMMdh  `  ``NMMMMMM+:   -smMMMMd-/+mMMMMh-`    -NNMNNMNmmmmdmmhs       
+echo   .+dMMMMMN+         `  yhMMMMMdh  `  ``NMMMMMM+:   `-hMMMMmsyhNMMMms      -NNMMNMN//-..--..       
+echo   -+dMMMMMNo   ``     ``shMMNMMdh     ``NMMMMMM/-     yNMMMMNNNMMMNy+  `   -NNMMMMN/-  `    ```    
+echo   -+dMNNMMNo   `     `  yhMMMMMdh  `  ``NMMMMMM/-     +yNMNNMMMMMMm:.      -NMMMNMN:-  `  ``       
+echo   .+dMMMMMmo..........` oyMMMMMmd``   -:NMNMMMM/.  `  :+NMMMMMMMMNd```     -MMMMMMN::..........`   
+echo   -+dMMMMMNdyyyhhyyhhd-`-/MMMMMMNso//:ydNMNMMNm-`  `  `.mNMMNMMMMhy     `` -NNMMMMMddhhhhdhhdhy:   
+echo  -+dMMMMMMMMMMMMMMMMM+: .hmMMMMNNNNNNMMNNMMNy/` `     `ydMMMMMMM+/  ```   -MMMNNMMMMMMMMMMMMNN+   
+echo ``-+dMMMMMMMMMMMMMMMMM+: `-/ymNNMMNNNMMNMNmh+-`        `oyMMMMMMM-.       `:NNMMMMMMMMMMMMMMMNN+   
+echo  ``-/o++++oo+oooo++++o-.   `.:/oshddddhho+:.     ``     .-++oo+++` `  ``  `.++o+++++++++++oo+++-   
+echo     ``````````````````        ```.......```          `    ```````       ``  ```````````````````    
+echo   `./++++++++//.`   ``  :/++/++++//+++:-``  `  ://+o/-`   //+++/////++++oo:- ./+o++:-     /+++o:.  
+echo   -odMMMNNMMMMMMNy+`   `mNMMMMMNNMMMMMo/  `` .:mMNNMNm:  `mNMMMMMMMMMNMMMNdy /hNMMNs+   ``NMMMMs-` 
+echo   -+dMNNMNmmdNNMMMmy:   mmMMMNNmmmmdmmo/`    :+mMNNMMM/` `ddmmmNNMMMMNNmmmhs :hNMMMs+   ``NMMMMs-` 
+echo   -+dMMNNh:--osNMMMM+`` dmMMMhs:-:::::.`     +hNMNMMMMo-` -:/::ydNMMNdo://-- :hNMMMs+    `mNMMMs-` 
+echo   -+dMMMmy   `.NMMMMs-` mmMMMy+` `   `      `oNNMMMNMMy+`      ohNMMmy: ``   :hNMMNs+    `mNMMMs-` 
+echo   -+dMMMmy`  ``NMMMMs-` mmMMMy+` `   `     `-yMMMmmNMMdh.      ohNMMmy:  `   :hNMMNs+   ``mNMMMs-` 
+echo   -+dMMMmy`  ``NMMMMs-` mmMMMdy//++/-      .+dMMM+sNNMNN.      ohMMMmy:      :hNMMNyo---::mNMMMs-` 
+echo   -+dMMMmy`  `.NMMMMo.` mmMMMNNmmNmd+      -ydNMM.:mNMMM:.     ohMMMmy:  `   :hNMNNNmmmmmdMMMMMs-` 
+echo   -+dMMMmy`  ``NMMMMo-` mmMMMMMMMMNm+  `   :dNMMM .hdMMM+/  `  ohMMMmy: ``   :hNMMMMMMMMMNMMMMMs-` 
+echo   -+dMMMmy`  ``NMMMMo-` mNMMMyo` ``     ```+NMMmd`.ohMMMys     ohMMMmy: ```  /hNMMMdhsssssNMMMMs-` 
+echo   -+dMMMmy   ``NMMMMo-` mmMMMy+` `    `` `/sMMMmdoohmMMMdh     ohNMMmy: ``   :hmMMNs+   ``mNMMMs-` 
+echo   -+dMMMmy   ``mNMMMs-` mmMMMy+`     `   `shMMMMMMMMMMMNNm.`   ohMMMmy:      :hNMMNs+   ``mNMMMs-` 
+echo   -+dMMNmy..`:+NMMMMo.` mmMMNho-...---`` .hmMMMdhyyydNMMMN-.   ohNMMmy: ``   /hNMMNs+   ``mNMMMs:` 
+echo   -+dMMMNmyyydmMMMNd/`  mmMMMmdhhhhhhh/- .NNMMN/-```+dNMMNo/   ohNMMmy:      /dNMMNs+   ``mNMMMs:` 
+echo  `-odMMMMMMMMMMNNdy-.   NNMMMMMMMMMNMMo:-:MMNNm-``` :ymMMMds ``sdMMMmy:      /dNMMNs+   ``NMMMMs-` 
+echo   ./shyyhhyyyyy+/-. ``  sshhyhhyyyyhyy:.:/hhhso. `  .:shhhs+   /oyyhy+-      -oyhhy+:   ``yyhhh/.` 
+echo    `````````````        ``````````````   ``````       `````` ``````````   `  ```````      `````` ``
+
+
+echo 1=new game 2=exit 3=load game
+set /p input=slect 1 or 2 or 3:
+if %input%==1 goto game
+if %input%==2 exit;
+if %input%==3 goto loadgame
+puase;
+:loadgame
+(
+set /p save=
+)<save.dlb
+echo 1=go to save this is your %save%
+set /p input=slect 1: 
+if %input%==1 goto %save%
+pause;
+:menu
+echo 1= load game
+set /p input=slect 1 or 2:
+if %input%==1 goto game
+if %input%==2 exit;
+pause;
+:1
+goto game
+:2
+goto bathroom
+:3
+goto lookagen
+:game
+echo ________________________________________________
+set save=1
+(
+echo %save%
+)>save.dlb 
+echo you are laying on a bed.
+echo outbed = get out of the bed
+echo nap = go back to bed
+set /p input=slect outbed or nap:
+if %input%==outbed goto outbed 
+if %input%==nap goto nap
+pause;
+:nap
+echo ________________________________________________
+echo you sleep for 8hr 
+pause;
+echo 1= go to bathroom
+echo 2= run to kichen
+set /p input=slect 1 or 2:
+if %input%==1 goto bathroom
+if %input%==2 goto kichenrun
+pause;
+goto outbed
+:outbed
+echo ____________________________________________________
+echo bathroom = go to the bathroom
+echo kichen = go to the kichen
+set /p input=slect bathroom or kichen:
+if %input%==bathroom goto bathroom
+if %input%==kichen goto kichenrun
+pause;
+:bathroom
+set save=2
+(
+echo %save%
+)>save.dLb 
+echo ____________________________________________________
+echo you go to the bath room and look around the room
+echo left = look left
+echo right = look right
+set /p input=slect left or right:
+if %input%==left goto bathroomleft
+if %input%==right goto bathroomright
+pause;
+:bathroomright
+echo ____________________________________________________
+set insanity=1
+(
+echo %insanity%
+)>insanity.dlb
+echo you see a somthing in the mear it is a monster dark red eyes in the dark 
+echo you run to the uther side of the room 
+pause;
+echo _____________________________________________________
+echo you gand 1 insanity
+echo your an insanety is %insanity%
+pause;
+echo _______________________________________________________
+echo your tarifed 
+echo 1= look agen 
+echo 2= rubit off
+echo 3= run to kichen
+set /p input=slect 1 or 2 or 3:
+if %input%==1 goto lookagen
+if %input%==2 goto rubitoff
+if %input%==3 goto kichenrun
+pause;
+:lookagen
+set save=3
+(
+echo %save%
+)>save.dlb
+echo _________________________________________________________
+echo you see a red glare in the mear 
+echo .....
+echo .....
+echo .....
+pause;
+echo _________________________________________________________
+echo has red eyes it has black ferr
+echo it has shap teth
+echo it looks at you
+pause;
+echo _________________________________________________________
+echo what do you do
+echo 1 = stay and  keep looking 
+echo 2 = run to the kichen 
+echo 3 =brak the mear 
+set  /p input=slect 1 or 2 or 3:
+if %input%==1 goto brakemear 
+if %input%==2 goto kichenrun
+if %input%==3 goto brakemear
+pause;
+:rubitoff
+echo __________________________________________________________
+echo you think it was nuthing and you keep doing what you were doing.
+echo what would like to do?
+echo 1 = go to kichen
+echo 2 = take a shit
+set  /p input=slect 1 or 2:
+if %input%==1 goto kichenrun
+if %input%==2 goto takeshit 
+pause;
+:kichenrun
+echo ___________________________________________________________
+echo you see a dead body on the flor
+set insanity=5
+echo you gand 4 insanity
+echo your insanity is %insanity%
+pause;
+echo _____________________________________________________________
+echo some thing kills you
+pause;
+echo _____________________________________________________________
+echo thank you for playing the game
+pause;
+exit;
+:brakemear
+echo _____________________________________________________________
+echo you go to brake the mear 
+echo ...
+echo ...
+echo ...
+echo a hand comes out of the mear 
+pause;
+echo ______________________________________________________________
+echo do you run
+echo 1 = stay
+echo 2 = run to kichen
+set /p input=slect 1 or 2
+if %input%==1 goto underdoom
+if %input%==2 goto kichenrun
+pause;
+:underdoom
+echo _______________________________________________________
+echo you win thank you for playing the demo (; <3
+pause;
+:look
+echo _______________________________________________________
+echo you look at the tolet ther is nuthing
+pause;
+echo __________________________________________________________
+echo 1= look right
+echo 2= go to kichen
+set /p input=slect 1 or 2 or 3:
+if %input%==1 goto bathroomright
+if %input%==2 goto kichenrun
+pause;
+:bathroomleft
+echo __________________________________________________________
+echo what would like to do?
+echo 1 = go to kichen
+echo 2 = look at the tolet
+echo 3 = go to bathroom
+set  /p input=slect 1 or 2 or 3:
+if %input%==1 goto kichenrun
+if %input%==2 goto look
+if %input%==3 goto bathroom
+pause;
+
+
+
+
+
+
+
+
